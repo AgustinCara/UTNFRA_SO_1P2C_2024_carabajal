4fb80480b88d7c1898bac2ae725a22066e467037
AgustinCara <agustin7.ch@gmail.com>
2024-10-08 23:07:31 +0000

    Punto_F.sh check

--- RTA_SCRIPT_Examen_20241004/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_F.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
+cd /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_carabajal/RTA_ARCHIVOS_Examen_20241008
 # Obtener IP pública
 IP_PUBLICA=$(curl -s ifconfig.me)
-echo "IP Pública: $IP_PUBLICA"
 
 # Obtener URL del repositorio remoto (suponiendo que estás en un repositorio git)
 REPO_URL=$(git config --get remote.origin.url)
-echo "URL del repositorio remoto: $REPO_URL"
 
-# Crear el directorio si no existe
-DIRECTORIO="/home/vagrant/repogit/UTNFRA_SO_1P2C_2024_carabajal/RTA_ARCHIVOS_Examen_20241008"
-mkdir -p "$DIRECTORIO"
 
 # Crear el archivo Filtro_Avanzado.txt
-ARCHIVO="$DIRECTORIO/Filtro_Avanzado.txt"
+sudo touch Filtro_Basico.txt
 
+echo "IP Pública: $IP_PUBLICA" >Filtro_Avanzado.txt
+echo "URL del repositorio remoto: $REPO_URL" >>Filtro_Avanzado.txt
 # Mostrar el contenido del archivo
-cat "$ARCHIVO"
+cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_carabajal/RTA_ARCHIVOS_Examen_20241008/Filtro_Avanzado.txt

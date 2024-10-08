edd72a81da9029dfcdd78ca2a2d406f49749ce6c
AgustinCara <agustin7.ch@gmail.com>
2024-10-08 18:39:36 +0000

    Punto_E check

---
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cd /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_carabajal/RTA_ARCHIVOS_Examen_20241008
+
+# Total de memoria RAM
+total_memoria=$(grep MemTotal /proc/meminfo | awk '{print $2 " " $3}')
+
+# Fabricante del chasis
+fabricante_chasis=$(sudo dmidecode -t chassis | grep "Manufacturer" | awk -F ': ' '{print $2}')
+
+# Escribir los resultados en el archivo Filtro_Basico.txt
+echo "Total de Memoria RAM: $total_memoria" > Filtro_Basico.txt
+echo "Fabricante del Chasis: $fabricante_chasis" >> Filtro_Basico.txt
+
+cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_carabajal/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
